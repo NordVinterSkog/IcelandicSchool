7ac259ebe2f11b78ec2746ce31f5f4d902051b77
Jan <kamilj_95@wp.pl>
2020-02-28 15:09:11 +0100

    Updates

---
+++ precache-manifest.a10bb3026c3212095b78fd3cc3517028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0405231782b5d63fb282a6486a979363",
+    "url": "/IcelandicSchool/index.html"
+  },
+  {
+    "revision": "b43f8764f089c00f860f",
+    "url": "/IcelandicSchool/static/css/main.2b25e9d9.chunk.css"
+  },
+  {
+    "revision": "f6513d897b1dd9e19369",
+    "url": "/IcelandicSchool/static/js/2.f86de377.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/IcelandicSchool/static/js/2.f86de377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43f8764f089c00f860f",
+    "url": "/IcelandicSchool/static/js/main.767b34c3.chunk.js"
+  },
+  {
+    "revision": "9db08f2dbe89ee6b173d",
+    "url": "/IcelandicSchool/static/js/runtime-main.732ce567.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/IcelandicSchool/precache-manifest.a10bb3026c3212095b78fd3cc3517028.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IcelandicSchool/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
